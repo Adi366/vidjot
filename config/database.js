617c3c4a7855162bdfb50bc402745f84a58e1105
Adi366 <centiplation@gmail.com>
2018-03-20 19:40:42 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: 'mongodb://Adi366:mlab@366@ds119449.mlab.com:19449/vidjot-prod' }
+    module.exports = { mongoURI: 'mongodb://Aditya:neorando@ds119449.mlab.com:19449/vidjot-prod' }
 } else {
     module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' }
 }
